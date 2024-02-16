3dc1f9a1aef04b0085469acd880c38015d54c46f
CarlosCanalesL <142610175+CarlosCanalesL@users.noreply.github.com>
2024-02-15 23:02:25 -0600

    dar formato al documento index

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import { sequelize } from "./database/connection.js";
 
 import './models/Author.js'
 
-async function main(){
-    try {
-        await sequelize.sync()
-        app.listen(4000);
-        console.log("Server is listening on port",4000);
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
+async function main() {
+  try {
+    await sequelize.sync()
+    app.listen(4000);
+    console.log("Server is listening on port", 4000);
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 }
 
 main();
