8e62adabd71babc090815d5575dc8c2c482315ad
CarlosCanalesL <142610175+CarlosCanalesL@users.noreply.github.com>
2024-02-15 23:01:37 -0600

    arreglo en connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,12 @@
-import Sequelize from "sequelize";
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('back_base', 'postgres', '1234', {
-    host: 'localhost',
-    dialect: 'postgres'
+// Cargar variables de entorno desde el archivo .env
+dotenv.config();
+
+// Crear una instancia de Sequelize utilizando las variables de entorno
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
 });
